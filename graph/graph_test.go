b326173862f9e98fb7d8ab572fcc6598f4134d27
lubingtan <lubingtan@126.com>
2019-03-15 19:18:38 +0800

    [Stage 1:build graph] add topology sort.

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -7,21 +7,26 @@ import (
 func Test4Graph4Init(t *testing.T) {
 	t.Logf("testing for graph initialize start.\n")
 	g := NewGraph()
-	v1 := NewVertex("0")
-	v2 := NewVertex("1")
-	v3 := NewVertex("2")
-	v4 := NewVertex("3")
+	v1 := NewVertex("name0")
+	v2 := NewVertex("name1")
+	v3 := NewVertex("name2")
+	v4 := NewVertex("name3")
 	g.AddEdge(v3, v4)
 	g.AddEdge(v1, v2)
 	g.AddEdge(v1, v3)
 	g.AddEdge(v2, v3)
 	t.Logf("Graph node number:%d\n", len(g.Vertexlist))
+	s, _ := g.TopoSort()
+	t.Log("=======sort=======")
+	for _, m := range s {
+		t.Log(m)
+	}
 	for _, v := range g.Vertexlist {
 		t.Log("=========Node=========")
 		t.Log("Index:", v.Index)
 		t.Log("Indegree:", v.Indegree)
 		t.Log("Outdegree:", v.Outdegree)
-		edge := v.FirstEdge
+		edge := v.OutEdge
 		for {
 			if nil == edge {
 				break
